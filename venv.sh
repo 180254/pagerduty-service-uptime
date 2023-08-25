7ba9a5d39f91dfab7907d4905a175fc3a8c8e4d4
Adrian Pedziwiatr <180254@users.noreply.github.com>
2023-08-25 20:29:25 +0200

    Minor code-style cleanup

--- venv.sh
+++ venv.sh
@@ -2,3 +2,4 @@
 python3 -m venv venv
 venv/bin/pip3 install --upgrade pip wheel setuptools
 venv/bin/pip3 install --disable-pip-version-check --upgrade -r requirements.txt
+venv/bin/pip3 install --disable-pip-version-check --upgrade -r requirements-dev.txt
